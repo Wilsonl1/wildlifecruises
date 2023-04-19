05a78a1c70e560bb82049a96a51403bd1de73bb1
Wilsonl1 <108264415+Wilsonl1@users.noreply.github.com>
2023-04-19 11:08:01 -0400

    ch 12 hands on proj

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 12
+      Project 12-01
+
+      Project to display a dropdown menu
+      Author: Lori Wilson
+      Date:   04/19/2023
+
+      Filename: project12-01.js
+*/
+
+$( () => {
+      $("li.submenu")
+      .mouseover(e => {
+            $(e.currentTarget).children("ul").show();
+      })
+
+      .mouseout(e => {
+            $(e.currentTarget).children("ul").hide();
+      })
+});
+
+
+
+
+                                                
